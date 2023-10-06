272d248fa2b00a394ea9c4a5f592c52cecfd0497
sekharpanda <34344111+sekharpanda@users.noreply.github.com>
2023-10-06 08:55:24 +0530

    Update dbconnect.js

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,6 +1,6 @@
 const  mongoose = require("mongoose");
 
-const url = "mongodb+srv://sekharp720:9238kZlskwV7LNoA@cluster0.j6l8e8c.mongodb.net/?retryWrites=true&w=majority";
+const url = "Put Your Mongodb Url";
 const connectdb = async ()=>{
    try {
       await mongoose.connect(url);
